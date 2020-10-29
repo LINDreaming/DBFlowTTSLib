b5e19f48ccd1d40858e28c3149ff4567a58b10b9
linxi <linxi@data-baker.com>
2020-10-29 20:52:10 +0800

    初始化Pod工程

---
+++ DBFlowTTSLib/DBFlowTTSLib/Classes/homeModel.h
@@ -0,0 +1,16 @@
+//
+//  homeModel.h
+//  DBFlowTTSLib
+//
+//  Created by linxi on 2020/10/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface homeModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DBFlowTTSLib/DBFlowTTSLib/Classes/homeModel.m
@@ -0,0 +1,12 @@
+//
+//  homeModel.m
+//  DBFlowTTSLib
+//
+//  Created by linxi on 2020/10/29.
+//
+
+#import "homeModel.h"
+
+@implementation homeModel
+
+@end
